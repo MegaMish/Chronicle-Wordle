b32a426ea6c29d1693cf61fd627ae4715e8bd4b0
MegaMish <36091436+MegaMish@users.noreply.github.com>
2023-08-11 23:44:44 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13667,20 +13667,20 @@ function flipTile(tile, index, array, guess) {
   )
 }
 
-// function getCorrectTiles(letter) {
-//   const tiles = guessGrid.querySelectorAll();
-//   const lastWord = new Array(5);
-//   for (let i = 0; i < 5; i++) {
-//     lastWord[i] = correctTiles[correctTiles.length - 1];
-//   }
-//   lastWord.forEach(tile => {
-//     if (targetWord.includes(tile.dataset.letter) && (targetWord.match(new RegExp(letter, "g") || [])).length === 1) {
-//       return false;
-//     } else if (targetWord.includes(letter)) {
-//       return true;
-//     }
-//   })
-// }
+function getCorrectTiles(letter) {
+  const tiles = guessGrid.querySelectorAll();
+  const lastWord = new Array(5);
+  for (let i = 0; i < 5; i++) {
+    lastWord[i] = correctTiles[correctTiles.length - 1];
+  }
+  lastWord.forEach(tile => {
+    if (targetWord.includes(tile.dataset.letter) && (targetWord.match(new RegExp(letter, "g") || [])).length === 1) {
+      return false;
+    } else if (targetWord.includes(letter)) {
+      return true;
+    }
+  })
+}
 
 function getActiveTiles() {
   return guessGrid.querySelectorAll("[data-state='active']")
